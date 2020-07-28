264b72ed6aab060334c9f7f7b93255da8960fcf2
Felipe Henrique <felipehfsouza@gmail.com>
2020-07-27 21:37:25 -0300

    Carrousel e footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import { categorias } from './data/dados_iniciais.json';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
       <Menu />
+      <BannerMain 
+        url={categorias[0].videos[0].url}
+        videoTitle={categorias[0].videos[0].titulo} 
+        videoDescription=""/>
+      { 
+        categorias.map((categoria, index) => (
+          <Carousel 
+            key={index}
+            ignoreFirstVideo
+            category={categoria}
+          />
+        ))
+      }
+          
+      <Footer />
     </div>
   );
 }
