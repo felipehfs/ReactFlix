69449ae01241c7184e649899fbf2f49598f358d0
Felipe Henrique <felipehfsouza@gmail.com>
2020-07-31 09:35:37 -0300

    Aula 4 - fetch/ajax servidor json-server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,42 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
-
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: '#000'
-  }
+    cor: '#000',
+  };
 
   const [categorias, setCategorias] = useState([]);
   const [values, setValues] = useState(valoresIniciais);
 
-
   function handleSubmit(event) {
     event.preventDefault();
 
-    setCategorias([{...values}, ...categorias]);
-
+    setCategorias([{ ...values }, ...categorias]);
+    setValues(valoresIniciais);
   }
+
+
+  useEffect(() => {
+    const URL_TOP = `http://localhost:8080/categorias`;
+    fetch(URL_TOP)
+      .then(async (response) => {
+        const body = await response.json();
+        setCategorias(body);
+      })
+  }, []);
 
 
   function onChangeField(event) {
     const { value, name } = event.target;
 
-      setValues({
-        ...values,
-        [name]: value
-      })
+    setValues({
+      ...values,
+      [name]: value,
+    });
   }
 
   return (
+    // eslint-disable-next-line react/jsx-filename-extension
     <PageDefault>
-      <h1>Cadastro de Categoria: {values.nome} </h1>
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
 
-      <form 
+      <form
         onSubmit={handleSubmit}
       >
 
@@ -48,15 +60,15 @@ function CadastroCategoria() {
           onChange={onChangeField}
         />
 
-        <FormField 
+        <FormField
           type="textarea"
           label="Descrição"
           name="descricao"
           value={values.descricao}
           onChange={onChangeField}
         />
-        
-        <FormField 
+
+        <FormField
           name="cor"
           label="Cor:"
           value={values.cor}
@@ -69,24 +81,25 @@ function CadastroCategoria() {
         </button>
       </form>
 
+      { categorias.length === 0 && (
+        <div>
+          Loading...
+        </div>
+      )}
 
       <ul>
-        {categorias.map((categoria, indice) => {
-          return (
-            <li key={`${categoria.nome}-${indice}`}>
-              {categoria.nome}
-            </li>
-          )
-        })
-
-        }
+        {categorias.map((categoria, indice) => (
+          <li key={`${categoria.nome}-${indice}`}>
+            {categoria.nome}
+          </li>
+        ))}
       </ul>
 
       <Link to="/">
         Ir para home
       </Link>
     </PageDefault>
-  )
+  );
 }
 
 export default CadastroCategoria;
